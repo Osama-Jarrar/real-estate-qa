764916d46079a6c9beb921a5c0af04dc04265990
Osama-jarrar <Osama.jarrar33@gmail.com>
2025-07-30 12:35:26 +0300

    Update main entrypoint for new pipeline structure

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # main.py
 
-from pipeline import run_data_pipeline
 
-def main():
-    processed_df = run_data_pipeline("Data/kc_house_data.csv")
-    print(processed_df.head())
+from pipelines.full_pipeline import full_pipeline
+
 
 if __name__ == "__main__":
-    main()
+
+    RAW_PATH = "Data/kc_house_data.csv"
+    
+
+    x = full_pipeline(RAW_PATH)
+    
+    print(x)
