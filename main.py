d61bfe82e3f22ea9f385e82957b8f5469acf4419
osama-jarrar <osama.jarrar33@gmail.com>
2025-08-08 19:55:39 +0300

    Update main.py to reflect pipeline integration changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # main.py
 
 
-from pipelines.full_pipeline import full_pipeline
+from rag.rag_pipeline import rag_pipeline
 
+from rag.retrieve import retrieve_ids_with_scores
 
 if __name__ == "__main__":
 
-    RAW_PATH = "Data/kc_house_data.csv"
+    RAW_PATH = "Data/enriched_properties.csv"
     
 
-    x = full_pipeline(RAW_PATH)
-    
+    x = rag_pipeline(RAW_PATH,"house less than 450k")
     print(x)
